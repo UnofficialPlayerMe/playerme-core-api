3873d5b78aac52d2a9d281f283ac6e79c2dc9e34
Zazz <captain.zazz@gmail.com>
2016-05-30 21:27:15 +0100

    Empty out settings, as its old data was moved into APIService

--- src/settings.js
+++ src/settings.js
@@ -1,13 +1,5 @@
-import JSONPRequestAdapter from './request/adapter/JSONPRequestAdapter'
-
 var settings = {
-    get adapter(){
-        return this._adapter;
-    },
-    set adapter(str){
-        this._adapter = str;
-    },
-    _adapter: JSONPRequestAdapter
+    
 };
 
 export default settings;
