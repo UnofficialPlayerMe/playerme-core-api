797846a6985031e6c9c5fab6256b7c263a1a71be
Zazz <captain.zazz@gmail.com>
2016-06-13 02:01:32 +0100

    Make entry select default adaptor based on environment

--- src/entry.js
+++ src/entry.js
@@ -6,17 +6,15 @@ import APIService              from './request/APIService';
 import RestfulJSRequestAdapter from './request/adapter/RestfulJSRequestAdapter';
 import JSONPRequestAdapter     from './request/adapter/JSONPRequestAdapter';
 
-var adapters = {
-    JSONP: JSONPRequestAdapter,
-    RestfulJS: RestfulJSRequestAdapter
-};
-
-// Set default adapter
-APIService.setAdapter(adapters.JSONP);
+// Environment setup
+if (window){ // If browser
+    APIService.setAdapter(JSONPRequestAdapter);
+} else { // If NodeJs
+    APIService.setAdapter(RestfulJSRequestAdapter);
+}
 
 export {
     settings,
-    adapters,
     APIService,
 
     UsersRepository
