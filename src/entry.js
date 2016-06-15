265bbde0f42423a310d09aed7bb5a06b00596234
Zazz <captain.zazz@gmail.com>
2016-06-15 08:43:13 +0100

    Check environment for the existence of Promise

--- src/entry.js
+++ src/entry.js
@@ -6,6 +6,18 @@ import APIService          from './request/APIService';
 import NodeRequestAdapter  from './request/adapter/NodeRequestAdapter';
 import JSONPRequestAdapter from './request/adapter/JSONPRequestAdapter';
 
+// Check for dependencies
+function checkDependency(variable, errorMessage){
+    if (typeof variable !== 'undefined') return;
+    if (typeof alert !== 'undefined'){
+        alert(errorMessage);
+    }else{
+        console.error(errorMessage);
+    }
+}
+
+checkDependency(Promise, "'Promise' isn't available on this platform and needs a pollyfill.");
+
 // Environment setup
 if (typeof window !== 'undefined'){ // If browser
     APIService.setAdapter(JSONPRequestAdapter);
